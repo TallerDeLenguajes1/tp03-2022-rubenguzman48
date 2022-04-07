60e23b6ed8b8e40a675c68baa27814093fc6b872
Ruben Guzman <rubenguzman48@gmail.com>
2022-04-07 19:10:03 -0300

    6to commit

--- tp3_1_2.cpp
+++ tp3_1_2.cpp
@@ -2,34 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char *Buff; //variable auxiliar
-    char *Nombre[4];
-    char *Apellido[4];
-    int i;
-    Buff= (char *) malloc(100*sizeof(char));
-    
-    for(i=0; i<=4; i++)
+int main() 
     {
-        printf("Ingrese sus nombres: ");
-        gets(Buff);
+        char *Buff; //variable auxiliar
+        char* Nombre[4];
+        int i;
+
+        Buff= (char *) malloc(100*sizeof(char));
         
-        Nombre= (char *) malloc((strlen(Buff)+1)*sizeof(char));
-        strcpy(Nombre[i],Buff);
+        for(i=0; i<=4; i++)
+        {
+            printf("Ingrese el nombre N° $i: ");
+            gets(Buff);
+            
+            Nombre= (char*) malloc((strlen(Buff)+1)*sizeof(char));
+            strcpy(Nombre[i],Buff);
+            
+            printf("escribiendo usando puts\n");
+            puts (Buff);
+            puts (Nombre[i]);
+            
+            //libero la memoria reservada en todos los punteros
+            free(Buff);
+            free(Nombre[i]);
         
-        
-        printf("escribiendo usando printf\n");
-        printf ("%s\n", Buff) ;
-        printf ("%s\n", Nombre) ;
-        
-        printf("escribiendo usando puts\n");
-        puts (Buff);
-        puts (Nombre[i]);
-        
-        //libero la memoria reservada en todos los punteros
-        free(Buff);
-        free(Nombre[i]);
-    
+            i++;
     }
     
     return 0;
