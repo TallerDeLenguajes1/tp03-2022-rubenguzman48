e07098f5e94e713367a68f0d91589f18277c3c8f
Ruben Guzman <rubenguzman48@gmail.com>
2022-04-07 18:18:55 -0300

    4to commit

--- tp3_1_1.cpp
+++ tp3_1_1.cpp
@@ -1,7 +1,8 @@
+//Trabajando con cadena de caracteres
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
 int main()
 {
     char frase [100];
@@ -15,8 +16,13 @@ int main()
     return 0;
 }
 */
-//Reserva de memoria
-///*
+//Trabajando con cadena de caracteres con reserva de memoria
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int main()
 {
     char *Buff;
@@ -29,4 +35,46 @@ int main()
 
     return 0;
 }
+*/
+
+//reserva dinámica de memoria con variable auxiliar
+
+///*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char *Buff; //variable auxiliar
+    char *Nombre;
+    char *Apellido;
+    Buff= (char *) malloc(100*sizeof(char));
+    
+    printf("Ingrese sus nombres: ");
+    gets(Buff);
+    
+    Nombre= (char *) malloc((strlen(Buff)+1)*sizeof(char));
+    strcpy(Nombre,Buff);
+    
+    printf("Ingrese sus Apellidos: ");
+    gets(Buff);
+    
+    Apellido= (char *) malloc((strlen(Buff)+1)*sizeof(char));
+    strcpy(Apellido,Buff);
+    
+    printf("escribiendo usando printf\n");
+    printf ("%s\n", Buff) ;
+    printf ("%s\n", Nombre) ;
+    printf ("%s\n", Apellido) ;
+    printf("escribiendo usando puts\n");
+    puts (Buff);
+    puts (Nombre);
+    puts (Apellido);
+    //libero la memoria reservada en todos los punteros
+    free(Buff);
+    free(Nombre);
+    free(Apellido);
+    return 0;
+}
+
 //*/
