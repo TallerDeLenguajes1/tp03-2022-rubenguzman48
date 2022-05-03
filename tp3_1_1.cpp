ee5fa8f3e1a35a54e57169135df13d3f79c11cef
Ruben Guzman <rubenguzman48@gmail.com>
2022-05-03 15:54:58 -0300

    hola

--- tp3_1_1.cpp
+++ tp3_1_1.cpp
@@ -39,7 +39,7 @@ int main()
 
 //reserva dinámica de memoria con variable auxiliar
 
-///*
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -77,4 +77,46 @@ int main() {
     return 0;
 }
 
-//*/
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+	 
+            char *buff;
+            char *nombre[5];
+             	         
+            buff=(char *)malloc(100*sizeof(char));
+    
+		    printf("Ingrese 5 nombres\n");
+		    printf("-----------------\n");
+			
+		    for(int i=0;i<5;i++)
+	        {
+			   printf("Ingrese el nombre %d: ",i+1);	
+			   gets(buff);   	
+			   
+			   nombre[i]=(char *)malloc((strlen(buff)+1)*sizeof(char));
+			   strcpy(nombre[i],buff);
+			}
+            
+            printf("\n\nListado de nombres\n");	
+			printf("-------------------\n");
+			
+			for(int i=0;i<5;i++)
+	        {
+		      printf("%d) ",i+1);
+		      puts(nombre[i]);
+		    }
+			 
+			for(int i=0;i<5;i++)
+	        { 
+		       free(nombre[i]);
+		    }
+			 
+            free(buff); 
+			 
+  return 0;
+}
